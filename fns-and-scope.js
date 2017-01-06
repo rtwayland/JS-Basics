8e0a80c6a349630c46c5df84ae689f00d9bbbb6f
raleigh wayland <rtwayland@gmail.com>
2017-01-06 13:36:27 -0700

    Completed problems 1-4

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -4,7 +4,9 @@ var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-  //Code Here
+function isTyler(name) {
+    return name === 'Tyler';
+}
 
 //////////////////PROBLEM 2////////////////////
 
@@ -12,7 +14,10 @@ var name = 'Tyler';
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
-  //Code Here
+function getName() {
+    var name = prompt('What is your name?');
+    return name;
+}
 
 
 //////////////////PROBLEM 3////////////////////
@@ -22,17 +27,16 @@ var name = 'Tyler';
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
-  //Code Here
+function welcome() {
+  alert('Welcome, ' + getName());
+}
 
 
 //////////////////PROBLEM 4////////////////////
 
-
-
-
 //What is the difference between arguments and parameters?
 
-  //Answer Here
+//Answer Here
 
 
 //////////////////PROBLEM 5////////////////////
@@ -42,7 +46,7 @@ var name = 'Tyler';
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
 
-  //Answer Here
+//Answer Here
 
 
 
@@ -52,13 +56,13 @@ var name = 'Tyler';
 
 //Create a function called myName that returns your name
 
-  //Code Here
-  
+//Code Here
+
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+//Code Here
 
 //Now alert the result of invoking newMyName
 
@@ -70,10 +74,10 @@ var name = 'Tyler';
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
-  //Code Here
+//Code Here
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
-  //Code Here
+//Code Here
 
 //Now invoke innerFn.
